33322189f95e5178e92218b4e0f1ec6d5ed9cab9
Kevin Fraser <kevin@ksfraser.ca>
2026-02-07 18:13:36 -0700

    Remove vendor directory from git tracking and add .gitignore

--- vendor/ksfraser/fa-hooks/count_braces.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$content = file_get_contents('src/Ksfraser/FA_Hooks/HookManager.php');
-echo 'Opening braces: ' . substr_count($content, '{') . PHP_EOL;
-echo 'Closing braces: ' . substr_count($content, '}') . PHP_EOL;
